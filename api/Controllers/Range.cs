3c7d2ef1f574593ffc7591e2c69c5778b375b6e9
Daniel Weslley <>
2018-08-20 22:52:47 +0100

    test failing

--- api/Controllers/Range.cs
+++ api/Controllers/Range.cs
@@ -5,13 +5,13 @@ using Faker.Extensions;
 
 namespace api.Controllers
 {
-    public class Range
-    {
-        public int Count { get; set; } = 10;
-        public bool Sort { get; set; } = false;
+	public class Range
+	{
+		public int Count { get; set; } = 10;
+		public bool Sort { get; set; } = false;
 
-        public IEnumerable<TItem> Of<TItem>(Func<TItem> generateItem)
-            => Count.Times(i => generateItem())
-                .OrderBy(n => Sort ? n : default(TItem));
-    }
+		public IEnumerable<TItem> Of<TItem>(Func<TItem> generateItem)
+		=> Count.Times(i => generateItem());
+		//.OrderBy(n => Sort ? n : default(TItem));
+	}
 }
